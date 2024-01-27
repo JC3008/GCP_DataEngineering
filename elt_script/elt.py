b79ddcd82570ee6c3004e7b2930d05cc766d97b7
JC3008 <josecarlos.ico@gmail.com>
2024-01-27 17:00:36 -0300

    auxiliar.py modified

--- elt_script/elt.py
+++ elt_script/elt.py
@@ -1,14 +1,7 @@
-from auxiliarcopy import extract
+from auxiliar import extract
 
 if __name__ == "__main__":
-    extract(
-    arquitecture=['local_source','datalake'],
-    source='local_json',
-    destination='landing',
-    filename='clientes.json',
-    context='case',
-    frequency='daily').mongo_to_gcp()
-    
+    print("here starts the csv elt")
     extract(
     arquitecture=['local_source','datalake'],
     source='local_csv',
@@ -16,4 +9,16 @@ if __name__ == "__main__":
     filename='empresa_alpha.csv',
     context='case',
     frequency='daily').transfer()
+    print("here ends the csv elt")
+    print("here starts the json elt")
+    extract(
+    arquitecture=['local_source','datalake'],
+    source='local_json',
+    destination='landing',
+    filename='clientes.json',
+    context='case',
+    frequency='daily').mongo_to_gcp()
+    print("here ends the csv elt")
+
+    
 
